0c93add23240d0eacad331d96904208bcc8797b6
Ramsey <browenbroz@gmail.com>
2024-03-25 11:03:16 +0300

    js0

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue, equal){
+    if("someValue=43blocks"){
+        return equal=1,8
+    }
+    }
+
+    function calculatesdistancesbelow42ndstreet(blocks, equal){
+        if("34blocks")
+        return equal=+8
+    }
+
+
+function distanceFromHqInFeet(value, equal){
+    if("value=43feet"){
+        return equal=264,2112
+    }
+}
+
+function distanceTravelledInFeet(start, destination, equal){
+if("start=43 destination=48"){
+    return equal=1320,2640,1584
+    
+}
+}
+function calculateFarePrice(start, destination,fare){
+    // declare empty variable fare
+    
+    if ("start = 0 && destination = 400" ){
+        console.log("free")
+}else if("start = 400 && destination = 2000"){
+    console.log(32)
+}else if("start = 2000 && destination = 2500"){
+    console.log("25 dollars")
+}else if(destination > 2500){
+    console.log("cannot travel that far")
+}
+}
